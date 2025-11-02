b98b0124de445366953e51066abd3eeef94b5bf3
Ramesh Manandhar <sayami00@gmail.com>
2025-11-02 07:16:32 +0545

    workflow created

---
+++ get_lib_versions.py
@@ -0,0 +1,21 @@
+import importlib.metadata
+packages = [
+    "langgraph",
+    "langchain_community",
+    "langchain_core",
+    "tavily-python",
+    "wikipedia",
+    "langchain-openai",
+    "langchain-google-genai",
+    "langchain-groq",
+    "structlog"
+    ]
+
+
+
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}=={version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pkg} (not installed)")
--- research_and_analyst/utils/model_loader.py
+++ research_and_analyst/utils/model_loader.py
@@ -9,6 +9,8 @@ from langchain_groq import ChatGroq
 from logger import GLOBAL_LOGGER as log
 from exception.custom_exception import ResearchAnalystException
 import asyncio
+from dotenv import load_dotenv
+
 
 class ApiKeyManager:
     def __init__(self):
@@ -72,7 +74,7 @@ class ModelLoader:
         Load and return the configured LLM model.
         """
         llm_block = self.config["llm"]
-        provider_key = os.getenv("LLM_PROVIDER", "google")
+        provider_key = os.getenv("LLM_PROVIDER", "openai")
 
         if provider_key not in llm_block:
             log.error("LLM provider not found in config", provider=provider_key)
